3a9fa2cdaefd4cc16b050ade46a76c9a41f31bb1
EmilyM <155667566+EmilyMenken@user.noreply.github.com>
2025-02-22 22:28:36 -0800

    changed html files to ejs, added admin.ejs, changes to app.js

--- app.js
+++ app.js
@@ -2,56 +2,60 @@ import express from 'express';
 
 const app = express();
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 const signatures = [];
 
 app.use(express.static('public'));
 
+// had an error but it was because I forgot to app.set
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 const PORT = 3000;
 
-//create the default route
-app.get('/', (req, res) =>{
-
-res.sendFile(`${import.meta.dirname}/views/home.html`);
+//render using ejs
+app.get('/', (req, res) => {
+    res.render(`home`);
 });
 
-app.get('/signed', (req, res) =>{
-
-    res.sendFile(`${import.meta.dirname}/views/signed.html`);
-    });
-
- app.post('/signed', (req, res) =>{
-
-const sign = {
-    fname: req.body.fname,
-    lname: req.body.lname,
-    jobTitle: req.body.jobTitle,
-    co: req.body.co,
-    linkedInURL: req.body.linkedInURL,
-    eAddress: req.body.eAddress,
-    other: req.body.other,
-    message: req.body.message,
-    mailingList: req.body.mailingList ? "Yes" : "No", //kept getting undefined and had to search up the issue, this fixes it by defining what should happen when someone doesn't check the mailingList
-    emailFormat: req.body.emailFormat || "Not specified" //also got undefined with this one
-};//end order
-
-  signatures.push(sign);
-  
-  console.log(signatures);
-
-  res.sendFile(`${import.meta.dirname}/views/signed.html`)
+//render using ejs
+app.get('/signed', (req, res) => {
+    res.render(`signed`), { async: true };
 });
 
-app.get('/admin/signatures', (req, res) => {
-    let html = '<h1>Signed! Thank you!</h1><ul>';
-    for(const sign of signatures) {
-        html += `<li>${sign.fname} ${sign.lname} - ${sign.jobTitle} - ${sign.co} - ${sign.linkedInURL} - ${sign.eAddress} - ${sign.other} - ${sign.message} - ${sign.mailingList} - ${sign.emailFormat}</li>`;
-
+app.post('/signed', (req, res) => {
+    
+    if (!req.body.fname || !req.body.lname || !req.body.eAddress) {
+        return res.send('Invalid Input: First Name, Last Name, and Email are required.');
     }
-html += '</ul>';
-res.send(html);
-// res.send(signatures);
+
+    const sign = {
+        fname: req.body.fname,
+        lname: req.body.lname,
+        jobTitle: req.body.jobTitle,
+        co: req.body.co,
+        linkedInURL: req.body.linkedInURL,
+        eAddress: req.body.eAddress,
+        weMetAt: req.body.weMetAt !== 'Please Select...' ? req.body.weMetAt : req.body.other || "Not specified",
+        other: req.body.other,
+        message: req.body.message,
+        mailingList: req.body.mailingList ? "Yes" : "No",
+        emailFormat: req.body.emailFormat || "Not specified",
+        timestamp: new Date().toLocaleString() // Add timestamp to each submission
+    };//end sign
+
+    signatures.push(sign);
+
+    console.log(signatures);
+
+//render using ejs
+    res.render('signed', { sign });
+});
+
+//render using ejs
+app.get('/admin', (req, res) => {
+    res.render('admin', { signatures });
 });
 
 app.listen(PORT, () => {
